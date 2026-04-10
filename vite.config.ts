bf0f40888057150599a22941831111f650a75b89
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2026-04-11 02:05:30 +0300

    Feat: gh-pages route fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({ base: './', plugins: [react(), tailwindcss()] });
+export default defineConfig({
+  base: './mobile-app/',
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'build'
+  }
+});
