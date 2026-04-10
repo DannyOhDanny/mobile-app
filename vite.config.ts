b7ae14554c8a394bfedb737363fdccc1fd41e3b5
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2026-04-11 02:06:43 +0300

    Feat: gh-pages route fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './mobile-app/',
+  base: '/mobile-app/',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'build'
